6e51a6c5e2c1619fbd15ef9003d3c493c8a6efc5
aballini <87954805+aballini@users.noreply.github.com>
2025-08-15 18:37:26 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro Civil CABA</title>
+    <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+    <div class="container">
+        <header>
+            <h1>Registro Civil CABA</h1>
+        </header>
+        <aside>
+            <nav>
+                <ul>
+                    <li><a href="proyectos25.html">Proyectos 25</a></li>
+                    <li><a href="proyectos26.html">Proyectos 26</a></li>
+                    <li><a href="satisfaccion.html">Satisfacción</a></li>
+                    <li><a href="disposiciones.html">Disposiciones</a></li>
+                    <li><a href="../autoridades/autoridades.">Autoridades</a></li>
+                </ul>
+            </nav>
+        </aside>
+        <main>
+            <p>Aqui desarrollaremos imagenes, informes, novedades del registro civil.</p>
+        </main>
+        <footer>
+            <p>direccionregistrocivil@buenosaires.gob.ar</p>
+        </footer>
+    </div>
+    <script src="rc.js"></script>
+</body>
+</html>
---
+++ portada.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const boton = document.getElementById("ingresar");
+    boton.addEventListener("click", () => {
+      window.location.href = "index.html";
+    });
+  });
+  
