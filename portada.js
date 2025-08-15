113f25f35f35a259006055927f92bd71b4093c96
aballini <87954805+aballini@users.noreply.github.com>
2025-08-15 19:25:50 -0300

    Delete portada.js

--- portada.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const boton = document.getElementById("ingresar");
-    boton.addEventListener("click", () => {
-      window.location.href = "index.html";
-    });
-  });
-  
